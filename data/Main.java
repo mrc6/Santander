64da9a7440cd3c9b4270b863e3b82a7f3ffa9b8f
mrc6 <marco.meireles.b@gmail.com>
2026-06-20 09:11:07 -0300

    Using LocalDateTime correctly

--- data/Main.java
+++ data/Main.java
@@ -2,15 +2,21 @@ import java.util.Date;
 import java.util.Calendar;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-public class Main{
+public class Main {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     Date date = new Date();
     System.out.println(date);
-    
+
     Calendar calendar = Calendar.getInstance();
-    DateFormat formartter = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss");
-    System.out.println(formartter.format(calendar.getTime()));
+    DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss");
+    System.out.println(formatter.format(calendar.getTime()));
+
+    LocalDateTime localDateTime = LocalDateTime.now();
+    DateTimeFormatter fmtr = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm:ss");
+    System.out.println(localDateTime.format(fmtr));
   }
 }
