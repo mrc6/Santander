36f07910603910720f67931040a35f2a3143a69f
mrc6 <marco.meireles.b@gmail.com>
2026-06-22 08:58:55 -0300

    Using LocalDateTime and LocalTime APIs

--- data/Main.java
+++ data/Main.java
@@ -3,6 +3,7 @@ import java.util.Calendar;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class Main {
@@ -15,8 +16,17 @@ public class Main {
     DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss");
     System.out.println(formatter.format(calendar.getTime()));
 
-    LocalDateTime localDateTime = LocalDateTime.now();
     DateTimeFormatter fmtr = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm:ss");
+    DateTimeFormatter fmt = DateTimeFormatter.ISO_DATE_TIME; 
+    DateTimeFormatter fmtmr = DateTimeFormatter.ofPattern("HH:mm:ss");
+    
+    LocalDateTime localDateTime = LocalDateTime.now();
+    
     System.out.println(localDateTime.format(fmtr));
+    System.out.println(localDateTime.format(fmt));
+    
+    LocalTime localTime = LocalTime.now();
+    
+    System.out.println(localTime.format(fmtmr));
   }
 }
